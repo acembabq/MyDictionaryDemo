179cf48b5b1d8d23cd78b112160c5c7b5c2c5aae
Seyit Acembaba <77851513+acembabq@users.noreply.github.com>
2021-02-19 00:12:37 +0300

    Add files via upload

---
+++ MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryDemo
+{
+    class MyDictionary<T>
+    {
+        public T[] _array;
+        public T[] _tempArray;
+
+        public MyDictionary()
+        {
+            _array = new T[0];
+        }
+
+        public void Add(T name) 
+        {
+            _tempArray = _array;
+            _array = new T[_array.Length + 1];
+
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+
+            _array[_array.Length - 1] = name;
+
+
+        }
+        public void List()
+        {
+            foreach (var item in _array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public int Count
+        {
+            get { return _array.Length; } // specifies the length of the array
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> listOfStudents = new MyDictionary<string>();
+
+            listOfStudents.Add("Eren Oktar");
+            listOfStudents.Add("Doğukan İmirzalıoğlu"); // Students
+            listOfStudents.Add("Cem Uzay");
+
+            listOfStudents.List(); // Students List
+
+            Console.WriteLine("\nAmount of students: "+listOfStudents.Count); //Amount Of Students
+
+        }
+    }
+}
